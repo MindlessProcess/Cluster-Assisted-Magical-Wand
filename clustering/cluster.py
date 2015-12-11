bd8d5713850b209122198c4fd2169799d8719e47
Lucas Merlette <lucas.merlette@gmail.com>
2015-12-11 12:14:42 +0100

    add: Folders clustering and processing with base classes

---
+++ clustering/cluster.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+
+class Cluster(object):
+    def __init__(self):
+        print 'Cluster'
---
+++ processing/processing.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+
+class Processing(object):
+    def __init__(self):
+        print 'Processing'
